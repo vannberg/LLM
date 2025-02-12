ece9971ffa99c7459020c5b5c4a158384ff8e8a3
Fredrik Vannberg <vannberg@gmail.com>
2025-02-12 16:34:30 -0500

    Update toybackprop4.py

--- toybackprop4.py
+++ toybackprop4.py
@@ -4,8 +4,15 @@ import os
 # Vocabulary definition
 common_words = [
     "the", "be", "to", "of", "and", "a", "in", "that", "have", "I",
-    # ... (rest of the words as before)
-    "because", "any", "these", "give", "day", "most", "us"
+    "it", "for", "not", "on", "with", "he", "as", "you", "do", "at",
+    "this", "but", "his", "by", "from", "they", "we", "say", "her", "she",
+    "or", "an", "will", "my", "one", "all", "would", "there", "their", "what",
+    "so", "up", "out", "if", "about", "who", "get", "which", "go", "me",
+    "when", "make", "can", "like", "time", "no", "just", "him", "know", "take",
+    "people", "into", "year", "your", "good", "some", "could", "them", "see", "other",
+    "than", "then", "now", "look", "only", "come", "its", "over", "think", "also",
+    "back", "after", "use", "two", "how", "our", "work", "first", "well", "way",
+    "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"
 ]
 
 digits = [str(i) for i in range(10)]
